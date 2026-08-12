34113d98a9aab71f0e42a17a18994be3c9dcf6e8
Pexideal <pexideal@outlook.com>
2026-08-12 17:29:56 +0200

    5
    5

--- public/client/client.js
+++ public/client/client.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   const hasToken = localStorage.getItem('pexideal_client_token') || localStorage.getItem('pexideal_token');
 
   if ((hasAuthObj && !Auth.isLoggedIn()) || (!hasAuthObj && !hasToken)) {
-    window.location.href = '/login.html';
+    window.location.href = 'login.html';
     return;
   }
 
